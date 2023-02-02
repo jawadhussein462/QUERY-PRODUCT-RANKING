792923d0777f77efac60d6a58cbe3b37901f6b4a
Hussein Jawad <jawadhussein462@gmail.com>
2023-02-02 15:13:32 +0100

    lemmatization = false

--- src/utils/support.py
+++ src/utils/support.py
@@ -6,18 +6,19 @@ import pandas as pd
 from pandas import DataFrame as D
 import spacy
 
+from src.utils.constant import CountryCode
 
 def get_spacy_from_country(country: str):
 
-    if country == "us":
+    if country == CountryCode.US.value:
         spacy_model = spacy.load("en_core_web_sm")
         spacy_stop_words = spacy_model.Defaults.stop_words
 
-    if country == "es":
+    if country == CountryCode.Spanish.value:
         spacy_model = spacy.load("es_core_news_sm")
         spacy_stop_words = spacy_model.Defaults.stop_words
 
-    if country == "jp":
+    if country == CountryCode.Japanese.value:
         spacy_model = spacy.load("ja_core_news_sm")
         spacy_stop_words = spacy_model.Defaults.stop_words
 
