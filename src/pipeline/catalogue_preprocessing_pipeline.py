b5b72f6bc5c3313e4f162c86063eb6b7712f8c0c
Hussein Jawad <jawadhussein462@gmail.com>
2023-02-09 14:23:11 +0100

    docstring

--- src/pipeline/catalogue_preprocessing_pipeline.py
+++ src/pipeline/catalogue_preprocessing_pipeline.py
@@ -1,10 +1,33 @@
-"""Prepare data in order to train the model."""
+"""
+Prepare product catalogue data for training by performing several data cleaning steps.
+
+Attributes
+---------
+    product_catalogue (pandas.DataFrame): The input data to be cleaned.
+
+Returns
+-------
+    pandas.DataFrame: The cleaned data.
+"""
+
 import numpy as np
 import regex as re
 from pandas import DataFrame as D
 
 
 def replace_nan_with_default(string: str, default_value: str = "") -> str:
+    """
+    Replace NaN values with the default value.
+
+    Args
+    ----
+        string (str): The input string to be checked and replaced.
+        default_value (str, optional): The value to replace NaN values with. Defaults to "".
+
+    Returns
+    -------
+        str: The cleaned string.
+    """
 
     if isinstance(string, float) and np.isnan(string):
         return ""
@@ -14,6 +37,17 @@ def replace_nan_with_default(string: str, default_value: str = "") -> str:
 
 
 def remove_html_tage(html_text: str) -> str:
+    """
+    Remove HTML tags from the input text.
+
+    Args
+    ----
+        html_text (str): The input text with HTML tags.
+
+    Returns
+    -------
+        str: The cleaned text without HTML tags.
+    """
 
     clean = re.compile("<.*?>")
 
@@ -23,6 +57,17 @@ def remove_html_tage(html_text: str) -> str:
 
 
 def remove_emojis(text: str):
+    """
+    Remove emojis from the input text.
+
+    Args
+    ----
+        text (str): The input text with emojis.
+
+    Returns
+    -------
+        str: The cleaned text without emojis.
+    """
 
     clean_text = re.sub(r"[^\p{L}\s]", "", text)
 
@@ -30,11 +75,21 @@ def remove_emojis(text: str):
 
 
 def run(product_catalgue: D) -> D:
+    """
+    Launch all the main steps of the data cleaning process.
 
-    """Launch all the mains steps of the module."""
+    Args
+    ----
+        product_catalogue (pandas.DataFrame): The input data to be cleaned.
+
+    Returns
+    -------
+        pandas.DataFrame: The cleaned data.
+    """
+
     df = product_catalgue.copy()
 
-    # str lower
+    # Convert to lower case
     df["product_title"] = df["product_title"].str.lower()
     df["product_description"] = df["product_description"].str.lower()
 
@@ -51,9 +106,5 @@ def run(product_catalgue: D) -> D:
     # Remove emojis
     df["product_title"] = df["product_title"].apply(remove_emojis)
     df["product_description"] = df["product_description"].apply(remove_emojis)
-
-    # lower case
-    df["product_title"] = df["product_title"].str.lower()
-    df["product_description"] = df["product_description"].str.lower()
 
     return df
