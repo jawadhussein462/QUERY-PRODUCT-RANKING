71a96b64309e1faa60bcd3d3cb6c34fef80d4186
Hussein Jawad <jawadhussein462@gmail.com>
2023-02-12 15:08:34 +0100

    update donwload_data

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-mkdir -p data
+while [ $# -gt 0 ]; do
+case "$1" in
+--data-folder=)
+data_folder="${1#=}"
+;;
+*)
+echo "Unknown option: $1"
+exit 1
+;;
+esac
+shift
+done
+
+if [ -z "$data_folder" ]; then
+echo "Error: --data-folder argument is required."
+exit 1
+fi
+
+mkdir -p "$data_folder"
 aicrowd login
-aicrowd dataset download -c esci-challenge-for-improving-product-search -o data 0 1 2 3
+aicrowd dataset download -c esci-challenge-for-improving-product-search -o "$data_folder" 0 1 2 3
+unzip "$data_folder/*.zip"
+
+
+
+
